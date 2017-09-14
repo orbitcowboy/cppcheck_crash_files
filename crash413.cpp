d15869581f0f5a86010a66bad71f95da2bf10291
orbitcowboy <ettl.martin78@gmail.com>
2017-09-14 16:00:10 +0200

    Added another crash file.

---
+++ crash413.cpp
@@ -0,0 +1,9 @@
+int sync_serial_ioctl_unlocked() {
+ int return_val = 0;
+ int dev;
+ if (dev) {
+    *R_DMA_CH4_CLR_INTR = IO_STATE(do) | IO_STATE(do);
+ } 
+ return return_val;
+}
+// from http://trac.cppcheck.net/ticket/8217
