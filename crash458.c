49f8e61175b0e537c7fc822b61fc4fc09e22d2c2
orbitcowboy <orbitcowboy@web.de>
2018-02-02 15:02:21 +0100

    Added another crash file, found by afl_cppcheck.

---
+++ crash458.c
@@ -0,0 +1,6 @@
+void f(){
+x= ={(continue continue { ( struct continue { ( ++ name5 name5 ) ( name5 name5 n
+ame5 ( name5 struct ( name5 name5 < ) ) ( default ) { name4 != name5 name5 name5
+ ( name5 name5 name5 ( { 1 >= void { ( ()) } 1 name3 return >= >= ( ) >= name5 (
+ name5 name6 :nam00 [ ()])}))})})})};
+}
