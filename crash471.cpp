f5d91c00de3020c14033b225564058fd093f38b6
orbitcowboy <orbitcowboy@web.de>
2018-07-01 00:51:35 +0200

    Added another crash file.

---
+++ crash471.cpp
@@ -0,0 +1,20 @@
+void fun()
+{
+	([]()
+	{
+		bool cond;
+		if (!cond)
+			return;
+		const int number;
+		switch (number)
+		{
+			case 1:
+			{
+				int *pointer;
+				if (++(*pointer) < 1)
+				{
+				}
+			}
+		}
+	});
+}
